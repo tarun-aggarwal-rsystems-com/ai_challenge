43b61d9e96a01245c5a241935d9d390540ca6032
HarshitaSrivastava-stack <Harshita.Srivastava@rsystems.com>
2026-05-14 00:24:38 +0530

    fix controller scan + docker

--- Server/src/main/java/com/rsystems/sdlc_copilot/SdlcCopilotApplication.java
+++ Server/src/main/java/com/rsystems/sdlc_copilot/SdlcCopilotApplication.java
@@ -3,7 +3,7 @@ package com.rsystems.sdlc_copilot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.rsystems.sdlc_copilot")
 public class SdlcCopilotApplication {
 
 	public static void main(String[] args) {
